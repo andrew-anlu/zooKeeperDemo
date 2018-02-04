01039333bc80f3d347a20d86387ea04f4113b1ef
anlu <357348329@qq.com>
2018-02-04 11:14:19 +0800

    commit

---
+++ src/main/java/com/anlu/zookeeper/App.java
@@ -0,0 +1,16 @@
+package com.anlu.zookeeper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static String ZOO_SERVER = "192.168.1.103:2181";
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+
+}
